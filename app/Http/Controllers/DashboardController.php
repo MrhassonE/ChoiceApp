e362f3ef771e44f0f85b90862c73f1cb7eacab2c
Hussein Ahmed <105203043+Maxwareiq@users.noreply.github.com>
2022-11-01 13:14:23 +0300

    Hasson

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Advertisement;
+use App\Models\Company;
+use App\Models\Department;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
-    {
-        return view('Dashboard.dashboard');
+    public function index(){
+        $departments = Department::all()->count();
+        $companies = Company::all()->count();
+        $ads = Advertisement::all()->count();
+        return view('Dashboard.dashboard',compact('departments','companies','ads'));
     }
 }
--- resources/views/Dashboard/dashboard.blade.php
+++ resources/views/Dashboard/dashboard.blade.php
@@ -1,163 +1,78 @@
 <x-appDash-layout>
-{{--    <div class="container-fluid">--}}
-{{--        <div class="row">--}}
-{{--            <div class="col-xl-6">--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-xl-6">--}}
-{{--                        <div class="card">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <div>--}}
-{{--                                    <div class="d-flex align-items-center">--}}
-{{--                                        <div class="avatar">--}}
-{{--                                            <div class="avatar-title rounded bg-soft-primary">--}}
-{{--                                                <i class="fa fa-newspaper icon nav-icon font-size-24 mb-0 text-primary"></i>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-6">
+                <div class="row">
+                    <div class="col-xl-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div>
+                                    <div class="d-flex align-items-center">
+                                        <div class="avatar">
+                                            <div class="avatar-title rounded bg-soft-primary">
+                                                <i class="fa fa-building icon nav-icon font-size-24 mb-0 text-primary"></i>
+                                            </div>
+                                        </div>
 
-{{--                                        <div class="flex-grow-1 ms-3">--}}
-{{--                                            <h6 class="d-flex mb-0 font-size-15">الأخبار</h6>--}}
-{{--                                            <h4 class="d-flex font-size-20">{{$news}}</h4>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
+                                        <div class="flex-grow-1 ms-3">
+                                            <h6 class="d-flex mb-0 font-size-15">عدد الاقسام الكلي</h6>
+                                            <h4 class="d-flex font-size-20">{{$departments}}</h4>
+                                        </div>
+                                    </div>
 
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-{{--                    <div class="col-xl-6">--}}
-{{--                        <div class="card">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <div>--}}
-{{--                                    <div class="d-flex align-items-center">--}}
-{{--                                        <div class="avatar">--}}
-{{--                                            <div class="avatar-title rounded bg-soft-primary">--}}
-{{--                                                <i class="bx bx bx bxs-briefcase-alt font-size-24 mb-0 text-primary"></i>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="flex-grow-1 ms-3">--}}
-{{--                                            <h6 class="d-flex mb-0 font-size-15">الخدمات</h6>--}}
-{{--                                            <h4 class="d-flex font-size-20">{{$services}}</h4>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-xl-6">--}}
-{{--                        <div class="card">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <div>--}}
-{{--                                    <div class="d-flex align-items-center">--}}
-{{--                                        <div class="avatar">--}}
-{{--                                            <div class="avatar-title rounded bg-soft-primary">--}}
-{{--                                                <i class="bx bx-package font-size-24 mb-0 text-primary"></i>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="flex-grow-1 ms-3">--}}
-{{--                                            <h6 class="mb-0 font-size-15">عدد الزيارات الكلي</h6>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div>--}}
-{{--                                        <h4 class="mt-4 pt-1 mb-0 font-size-22">{{showAmount($visitors->visit_all)}}</h4>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-xl-6">--}}
-{{--                        <div class="card">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <div>--}}
-{{--                                    <div class="d-flex align-items-center">--}}
-{{--                                        <div class="avatar">--}}
-{{--                                            <div class="avatar-title rounded bg-soft-primary">--}}
-{{--                                                <i class="bx bx-package font-size-24 mb-0 text-primary"></i>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="flex-grow-1 ms-3">--}}
-{{--                                            <h6 class="mb-0 font-size-15">عدد الزيارات لهذا الشهر</h6>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div>--}}
-{{--                                        <h4 class="mt-4 pt-1 mb-0 font-size-22">{{showAmount($visitors->visit_monthly)}}</h4>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
+                    <div class="col-xl-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div>
+                                    <div class="d-flex align-items-center">
+                                        <div class="avatar">
+                                            <div class="avatar-title rounded bg-soft-primary">
+                                                <i class="bx bx bx bxs-briefcase-alt font-size-24 mb-0 text-primary"></i>
+                                            </div>
+                                        </div>
+                                        <div class="flex-grow-1 ms-3">
+                                            <h6 class="d-flex mb-0 font-size-15">عدد الشركات الكلي</h6>
+                                            <h4 class="d-flex font-size-20">{{$companies}}</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-{{--            <div class="col-xl-6">--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-xl-6">--}}
-{{--                        <div class="card">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <div>--}}
-{{--                                    <div class="d-flex align-items-center">--}}
-{{--                                        <div class="avatar">--}}
-{{--                                            <div class="avatar-title rounded bg-soft-primary">--}}
-{{--                                                <i class="fa fa-calendar-alt font-size-24 text-primary"></i>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="flex-grow-1 ms-3">--}}
-{{--                                            <h6 class="d-flex mb-0 font-size-15">الندوات</h6>--}}
-{{--                                            <h4 class="d-flex font-size-20">{{$seminars}}</h4>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-xl-6">--}}
-{{--                        <div class="card">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <div>--}}
-{{--                                    <div class="d-flex align-items-center">--}}
-{{--                                        <div class="avatar">--}}
-{{--                                            <div class="avatar-title rounded bg-soft-primary">--}}
-{{--                                                <i class="fa fa-lightbulb font-size-24 mb-0 text-primary"></i>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="flex-grow-1 ms-3">--}}
-{{--                                            <h6 class="d-flex mb-0 font-size-15">الأنجازات </h6>--}}
-{{--                                            <h4 class="d-flex font-size-20">{{showAmount($achievements)}}</h4>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-xl-6">--}}
-{{--                        <div class="card">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <div>--}}
-{{--                                    <div class="d-flex align-items-center">--}}
-{{--                                        <div class="avatar">--}}
-{{--                                            <div class="avatar-title rounded bg-soft-primary">--}}
-{{--                                                <i class="bx bx-package font-size-24 mb-0 text-primary"></i>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="flex-grow-1 ms-3">--}}
-{{--                                            <h6 class="mb-0 font-size-15">عدد الزيارات لهذا اليوم</h6>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div>--}}
-{{--                                        <h4 class="mt-4 pt-1 mb-0 font-size-22">{{showAmount($visitors->visit_daily)}}</h4>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+            <div class="col-xl-6">
+                <div class="row">
+                    <div class="col-xl-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div>
+                                    <div class="d-flex align-items-center">
+                                        <div class="avatar">
+                                            <div class="avatar-title rounded bg-soft-primary">
+                                                <i class="fa fa-ad font-size-24 text-primary"></i>
+                                            </div>
+                                        </div>
+                                        <div class="flex-grow-1 ms-3">
+                                            <h6 class="d-flex mb-0 font-size-15">الاعلانات</h6>
+                                            <h4 class="d-flex font-size-20">{{$ads}}</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 
     <a href="#" data-bs-toggle="modal" data-bs-target=".support"
        style="background:#213A75;padding: 20px;font-size: 20px;position: fixed; opacity: 0.8; bottom: 30px;left: 20px;color: #fff;border-radius: 50%;z-index: 10000;"
