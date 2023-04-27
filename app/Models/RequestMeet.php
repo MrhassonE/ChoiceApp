f901a769feef05bc17c031191643919a66d2a460
Hussein Ahmed <105203043+Maxwareiq@users.noreply.github.com>
2023-04-27 03:23:28 +0300

    Hasson2

--- app/Models/RequestMeet.php
+++ app/Models/RequestMeet.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class RequestMeet extends Model
 {
     use HasFactory;
+    protected $fillable = ['id','service_id','company_id','user_id','date','time'];
+
+    public function CompanyService(){
+        return $this->belongsTo(CompanyService::class,'service_id');
+    }
+    public function Company(){
+        return $this->belongsTo(Company::class,'company_id');
+    }
+    public function User(){
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
