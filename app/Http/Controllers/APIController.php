046cc696d45d3097eb1ab0c519b0de3a3232bf5d
Zangestu <59559955+zangestu2Eszh@users.noreply.github.com>
2023-05-09 19:21:37 +0300

    choice new

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -218,7 +218,7 @@ class APIController extends Controller
             return $q->where('is_active',1)->get();
         })->get(["id","name"]);
 
-        $profile = User::where('id',3)//Auth::guard('api')->user()->id)
+        $profile = User::where('id',Auth::guard('api')->user()->id)
             ->get();
         $setting= GeneralSetting::get(['company_name','company_logo','email','phone','phone2','facebook','instagram','telegram','whatsapp','policy','conditions','android_app','ios_app']);
         $whatsNew = WhatsNew::get(['title']);
